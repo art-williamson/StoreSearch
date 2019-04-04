6a3440738dd18a7be8393eecfdad83fc03f53dfe
Art Williamson <alwilliamson@xactware.com>
2019-04-04 17:06:46 -0600

    added network calls and ability to parse JSON objects

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -6,7 +6,29 @@
 //  Copyright © 2019 Art Williamson. All rights reserved.
 //
 
-class SearchResult {
-    var name = ""
+class SearchResult: Codable, CustomStringConvertible {
+
+    var trackName = ""
     var artistName = ""
+    var kind = ""
+    var trackPrice = 0.0
+    var currency = ""
+    var artworkUrl60 = ""
+    var artworkUrl100 = ""
+    var trackViewUrl = ""
+    var primaryGenreName = ""
+
+    var name: String {
+        return trackName
+    }
+
+    var description:String {
+        return "Kind: \(kind), Name: \(name), Artist Name: \(artistName)"
+    }
+}
+
+class ResultArray: Codable {
+
+    var resultCount = 0
+    var results = [SearchResult]()
 }
