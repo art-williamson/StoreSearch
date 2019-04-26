e1496b85fb0c4d0bcb980789cce80850f00c429b
Art Williamson <alwilliamson@xactware.com>
2019-04-26 08:49:38 -0600

    added landscape view controller and set it up to display on rotation. Animated the transition.

--- StoreSearch/View Controllers/SearchViewController.swift
+++ StoreSearch/View Controllers/SearchViewController.swift
@@ -14,6 +14,7 @@ class SearchViewController: UIViewController {
     var hasSearched = false
     var isLoading = false
     var dataTask: URLSessionDataTask?
+    var landscapeVC: LandscapeViewController?
 
     //MARK: Outlets
 
@@ -57,6 +58,16 @@ class SearchViewController: UIViewController {
         }
     }
 
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        switch newCollection.verticalSizeClass {
+        case .compact:
+            showLandscape(with: coordinator)
+        case .regular, .unspecified:
+            hideLandscape(with: coordinator)
+        }
+    }
+
     // MARK:- Private Methods
     func iTunesURL(searchText: String, category: Int) -> URL {
         let kind: String
@@ -89,6 +100,30 @@ class SearchViewController: UIViewController {
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         present(alert, animated: true, completion: nil)
         alert.addAction(action)
+    }
+
+    func showLandscape(with coordinator: UIViewControllerTransitionCoordinator) {
+        guard landscapeVC == nil else { return }
+        landscapeVC = storyboard!.instantiateViewController(withIdentifier: "LandscapeViewController") as? LandscapeViewController
+        if let controller = landscapeVC {
+            controller.view.frame = view.bounds
+            controller.view.alpha = 0
+            view.addSubview(controller.view)
+            addChild(controller)
+            coordinator.animate(alongsideTransition: { _ in controller.view.alpha = 1 }, completion: { _ in controller.didMove(toParent: self)})
+        }
+    }
+
+    func hideLandscape(with coordinator: UIViewControllerTransitionCoordinator) {
+        if let controller = landscapeVC {
+            controller.willMove(toParent: nil)
+            coordinator.animate(alongsideTransition: { _ in controller.view.alpha = 0 },
+                                completion: { _ in
+                                    controller.view.removeFromSuperview()
+                                    controller.removeFromParent()
+                                    self.landscapeVC = nil
+            })
+        }
     }
 }
 
